08ca0c28ff2229707d794c36394aa385d5a46a88
Anh Nông Dân <trung10052014@gmail.com>
2022-12-21 10:32:41 +0700

    implement upload file

---
+++ src/main/java/com/example/maemaeshop/maemaeshop/services/IStorageService.java
@@ -0,0 +1,13 @@
+package com.example.maemaeshop.maemaeshop.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public interface IStorageService {
+    public String storeFile(MultipartFile file);
+    public Stream<Path> loadAll();
+    public byte[] readFileContent(String filename);
+    public void deleteAllFiles();
+}
